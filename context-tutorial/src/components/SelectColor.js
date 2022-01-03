980798f68f36b459922ffbca7dd024a25a038935
LAPTOP-ICBL9CQ4\비플러스랩 <beeblu.0@gmail.com>
2022-01-03 13:12:50 +0900

    static contextType with class component

--- context-tutorial/src/components/SelectColor.js
+++ context-tutorial/src/components/SelectColor.js
@@ -1,33 +1,38 @@
-import React from "react";
-import { ColorConsumer } from "../contexts/color";
+import React, { Component } from "react";
+import colorContext from "../contexts/color";
 const colors = ["red", "orange", "yellow", "green", "blue", "indigo", "purple"];
-export default function SelectColor() {
-  return (
-    <div>
-      <h2>색상을 선택하세요.</h2>
-      <ColorConsumer>
-        {({ actions }) => (
-          <div style={{ display: "flex" }}>
-            {colors.map((color) => (
-              <div
-                key={color}
-                style={{
-                  background: color,
-                  width: "24px",
-                  height: "24px",
-                  cursor: "pointer",
-                }}
-                onClick={() => actions.setColor(color)}
-                onContextMenu={(e) => {
-                  e.preventDefault();
-                  actions.setSubcolor(color);
-                }}
-              />
-            ))}
-          </div>
-        )}
-      </ColorConsumer>
-      <hr />
-    </div>
-  );
+export default class SelectColor extends Component {
+  static contextType = colorContext;
+  handleSetColor = (color) => {
+    this.context.actions.setColor(color);
+  };
+  handleSetsubcolor = (subcolor) => {
+    this.context.actions.setSubcolor(subcolor);
+  };
+  render() {
+    return (
+      <div>
+        <h2>색상을 선택하세요.</h2>
+        <div style={{ display: "flex" }}>
+          {colors.map((color) => (
+            <div
+              key={color}
+              style={{
+                background: color,
+                width: "24px",
+                height: "24px",
+                cursor: "pointer",
+              }}
+              onClick={() => this.handleSetColor(color)}
+              onContextMenu={(e) => {
+                e.preventDefault();
+                this.handleSetsubcolor(color);
+              }}
+            />
+          ))}
+        </div>
+        <hr />
+      </div>
+    );
+  }
 }
